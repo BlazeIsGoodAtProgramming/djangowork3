d1be315382f5b3abd345be7f3b11cd809608a727
BlazeIsGoodAtProgramming <154691571+BlazeIsGoodAtProgramming@users.noreply.github.com>
2024-05-22 22:20:01 +0330

    create

---
+++ root/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Services,Team,Skill,Portfolio
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['title','percent']
+
+
+admin.site.register(Services)
+
+admin.site.register(Team)
+
+admin.site.register(Skill,SkillAdmin)
+
+admin.site.register(Portfolio)
+
+# Register your models here.
